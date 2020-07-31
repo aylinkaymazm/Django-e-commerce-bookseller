a4f0e780ab4667663a5fa077559411192d3b0057
maleficenta <neskahvekokusu@gmail.com>
2020-08-01 01:39:23 +0300

    list display hatası giderildi

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'amount','status']
+    list_filter = ['status']
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -18,3 +18,24 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+class Product(models.Model):
+    STATUS= (
+        ('True' , 'Evet'),
+        ('False', 'Hayır'),
+    )
+    #product ın category ile ilişkisi var 1-1 manytomany
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    keywords= models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image=models.ImageField(blank=True,upload_to='images/')
+    price=models.FloatField()
+    amount=models.IntegerField()
+    detail=models.TextField()
+    status=models.CharField(max_length=10,choices=STATUS)
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title

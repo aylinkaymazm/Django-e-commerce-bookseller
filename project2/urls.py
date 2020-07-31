479a6a119ac19b0291978e8a1d221d32693228db
maleficenta <neskahvekokusu@gmail.com>
2020-07-31 18:02:29 +0300

    superuser eklendi

--- project2/urls.py
+++ project2/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = {
+urlpatterns = [
     #yolu yazmalısın
     path('', include('home.urls')),
     path('home/', include('home.urls')),
     path('product/', include('product.urls')),
     path('admin/', admin.site.urls),
-}
+]

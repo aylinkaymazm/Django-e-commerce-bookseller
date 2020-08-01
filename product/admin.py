cc72900110b8442030a87e240efcb06e62d909a5
maleficenta <neskahvekokusu@gmail.com>
2020-08-02 00:43:19 +0300

    image_tag eklendi

--- product/admin.py
+++ product/admin.py
@@ -1,15 +1,29 @@
 from django.contrib import admin
 
-from .models import Category, Product
+from product.models import Category, Product, Images
+
+
+class ProductImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'price', 'amount','status']
-    list_filter = ['status']
 
-admin.site.register(Category,CategoryAdmin)
-admin.site.register(Product,ProductAdmin)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'amount','image_tag', 'status']
+    readonly_fields = ('image_tag', )
+    list_filter = ['status', 'category']
+    inlines = [ProductImageInline]
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product', 'image']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Images, ImagesAdmin)
--- project2/settings.py
+++ project2/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL='/media/'
+MEDIA_URL='/uploads/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'uploads')
